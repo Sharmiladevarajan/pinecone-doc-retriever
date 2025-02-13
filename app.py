c25101d932c318526459d0aa4bfc4530a7339b95
sharmiladevarajan <devarajsharmila36@gmail.com>
2025-02-13 19:10:24 +0530

    changes

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from apis.retrive_doc import retrive_doc
+# from apis.retrive_doc import retrive_doc
 from apis.retrive import retrive_record
 
 app = Flask(__name__)
 CORS(app) 
 
 
-@app.route('/retrive_document', methods=['POST'])
-def retrive_data(): 
-    try:
-        return jsonify({"data":retrive_doc(request.get_json()["query"]),"status_code":200})
-    except Exception as e:
-        return jsonify({"data":e,"status_code":400})
+# @app.route('/retrive_document', methods=['POST'])
+# def retrive_data(): 
+#     try:
+#         return jsonify({"data":retrive_doc(request.get_json()["query"]),"status_code":200})
+#     except Exception as e:
+#         return jsonify({"data":e,"status_code":400})
 @app.route('/retrive_record', methods=['POST'])
 def retrive_data(): 
     try:
